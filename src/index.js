bb4c2bba42b6e51fed3a73a23cdcb08feee58843
reaget <reaget.acc@gmail.com>
2024-03-18 20:55:19 +0300

    chore: remove comments

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/test"> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>

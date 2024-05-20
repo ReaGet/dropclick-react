f44044d44f66c5d2116fc542990f319313d12814
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-05-20 07:51:13 +0300

    chore: last update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -131,7 +131,7 @@ const HomePage = () => {
             ) : (
               <div className="flex flex-col gap-6 w-full max-w-[700px] sm:px-8 text-xl sm:text-2xl text-white text-center">
                 <span>Для просмотра гайдов нужно приобрести подписку. </span>
-                <span>Вы можете сделать это в<Link to="/account" className="text-primary">личном кабинете</Link></span>
+                <span>Вы можете сделать это в <Link to="/account" className="text-primary">личном кабинете</Link></span>
               </div>
             )
         }
